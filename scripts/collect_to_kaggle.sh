7674b118ffd2c8b063d574751cc55d57dc41a75f
SirWerto <sirwerto@gmail.com>
2025-12-08 15:45:57 +0100

    Environment for python script

--- scripts/collect_to_kaggle.sh
+++ scripts/collect_to_kaggle.sh
@@ -13,6 +13,11 @@ TMP_FOLDER=$(mktemp --directory)
 
 echo "Start collection"
 
+echo "Create env"
+python -m venv $TMP_FOLDER/venv
+source $TMP_FOLDER/venv/bin/activate
+pip install -r requirements.txt
+
 echo "1 - Fetch server list"
 SERVER_LIST_FILE_PATH=$TMP_FOLDER/servers.csv
 time sh $SCRIPTS_PATH/generate_travian_subdomain_list.sh $SERVER_LIST_FILE_PATH
@@ -27,4 +32,5 @@ time python $SCRIPTS_PATH/clean_raw_map_sql.py $SERVER_LIST_FILE_PATH $SNAPSHOT_
 echo "4 - Push snapshots to Kaggle"
 sh $SCRIPTS_PATH/snapshot_to_kaggle.sh $TRAVIAN_SNAPSHOT_FOLER_PATH $PRIV_PATH/travian_map_sql_snapshot.json
 
+deactivate
 rm -r $TMP_FOLDER
